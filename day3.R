9e7658e2b2a75c7d51b27014b3157ea3d95f6844
Jonas Hughes Larsen <jhularsen@gmail.com>
2022-12-03 17:52:22 +0100

    small code update

--- day3.R
+++ day3.R
@@ -3,8 +3,7 @@ input <- tibble(x = read_lines("data/day3_input.txt"))
 
 # star 1
 input %>% 
-  mutate(bag = row_number(),
-         first_half = substring(x, 1, nchar(x)/2),
+  mutate(first_half = substring(x, 1, nchar(x)/2),
          second_half = substring(x, nchar(x)/2 + 1, nchar(x)),
          first_half_letters = str_extract_all(first_half, "[A-z]"),
          second_half_letters = str_extract_all(second_half, "[A-z]")) %>% 
@@ -12,15 +11,14 @@ input %>%
   unnest(second_half_letters) %>% 
   filter(first_half_letters == second_half_letters) %>% 
   unique() %>% 
-  group_by(bag) %>% 
+  rowwise() %>% 
   mutate(prio = which(first_half_letters == c(letters, LETTERS))) %>% 
   ungroup() %>% 
   summarise(res = sum(prio))
 
 # star 2
 input %>% 
-  mutate(bag = row_number(),
-         group = (bag + 2) %/% 3) %>% 
+  mutate(group = (row_number() + 2) %/% 3) %>% 
   group_by(group) %>% 
   mutate(bag_group = c("bag1", "bag2", "bag3")) %>% 
   pivot_wider(id_cols = group, names_from = bag_group, values_from = x) %>% 
